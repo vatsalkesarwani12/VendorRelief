5fec87fc79df9de76aba5a2bb41edcbb12876414
Poornirt <47494111+Poornirt@users.noreply.github.com>
2021-01-10 21:34:50 +0530

    Checked internet availability  (#34)
    * Merge branch 'develop' of /Users/poornima/AndroidStudioProjects/VendorRelief with conflicts.
    
    * Checked internet availability before showing web view(CoronaSpreadDataFragment)

--- app/src/main/java/com/CodeNaroNa/vendor/relief/CoronaSpreadDataFragment.java
+++ app/src/main/java/com/CodeNaroNa/vendor/relief/CoronaSpreadDataFragment.java
@@ -6,26 +6,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.CodeNaroNa.vendor.relief.R;
+import com.CodeNaroNa.vendor.relief.Helper.NetworkCheckHelper;
 
 
 public class CoronaSpreadDataFragment extends Fragment {
 
     private WebView web;
+    private TextView noInternetView;
+    private NetworkCheckHelper networkCheckHelper;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crona_spread_data, container,false);
         web=view.findViewById(R.id.web);
-        web.loadUrl("https://vendor-relief.web.app/data.html");
-        WebSettings webSettings = web.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        noInternetView = view.findViewById(R.id.no_internet);
+        networkCheckHelper = new NetworkCheckHelper(getActivity());
+        if(networkCheckHelper.checkDeviceConnectedToInternet()) {
+            web.loadUrl("https://vendor-relief.web.app/data.html");
+            WebSettings webSettings = web.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            noInternetView.setVisibility(View.GONE);
+        } else {
+            noInternetView.setVisibility(View.VISIBLE);
+        }
 
         return view;
     }
